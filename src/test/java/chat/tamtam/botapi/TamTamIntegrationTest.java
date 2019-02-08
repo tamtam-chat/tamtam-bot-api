daae85639768a7212ebbecb44cb31fa14cb0f7a6
Alexander Chuprin <alexander.chuprin@corp.mail.ru>
2019-02-08 11:04:18 +0300

    SendMessageQuery integration test

--- src/test/java/chat/tamtam/botapi/TamTamIntegrationTest.java
+++ src/test/java/chat/tamtam/botapi/TamTamIntegrationTest.java
@@ -1,8 +1,9 @@
 package chat.tamtam.botapi;
 
 import java.lang.invoke.MethodHandles;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
-import org.junit.Before;
 import org.junit.experimental.categories.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,29 +11,33 @@ import org.slf4j.LoggerFactory;
 import chat.tamtam.botapi.client.TamTamClient;
 import chat.tamtam.botapi.client.impl.JacksonSerializer;
 import chat.tamtam.botapi.client.impl.OkHttpTransportClient;
-import chat.tamtam.botapi.model.User;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import chat.tamtam.botapi.exceptions.APIException;
+import chat.tamtam.botapi.exceptions.ClientException;
+import chat.tamtam.botapi.model.Chat;
+import chat.tamtam.botapi.model.ChatList;
+import chat.tamtam.botapi.model.UserWithPhoto;
 
 /**
  * @author alexandrchuprin
  */
 @Category(IntegrationTest.class)
 public abstract class TamTamIntegrationTest {
-    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private TamTamClient client = new TamTamClient(getToken(), new OkHttpTransportClient(), new JacksonSerializer());
 
     protected TamTamBotAPI botAPI = new TamTamBotAPI(client);
     protected TamTamUploadAPI uploadAPI = new TamTamUploadAPI(client);
-    User me;
 
-    @Before
-    public void setUp() throws Exception {
-        me = botAPI.getMyInfo().execute();
-        assertThat(me, is(notNullValue()));
-        LOG.info("Current bot: " + me);
+    protected UserWithPhoto getMe() throws APIException, ClientException {
+        return botAPI.getMyInfo().execute();
+    }
+
+    protected List<Chat> getChats() throws APIException, ClientException {
+        ChatList chatList = botAPI.getChats().count(10).execute();
+        return chatList.getChats();
+    }
+
+    protected static <T> T random(List<T> list) {
+        return list.get(ThreadLocalRandom.current().nextInt(list.size()));
     }
 
     private static String getToken() {
